e726f2b7fdcc7933230b14700dc517c9c5eca1fc
sultondev <sultondeveloper@gmail.com>
2023-02-28 21:39:46 +0500

    fix: refactored code

--- src/store/useContacts.ts
+++ src/store/useContacts.ts
@@ -38,8 +38,13 @@ export const useContactsStore: any = defineStore("contactsStore",  {
     deleteContactTag(tagId: string, contactId: string) {
       const indexOfContact = this.contacts?.findIndex((contact) => contact.id === contactId)
       const indexOfTagInContact = this.contacts[indexOfContact].tags?.findIndex((tag) => tag.id === tagId)
-      this.contacts[indexOfContact].tags = [...this.contacts[indexOfContact]?.tags.slice(0, indexOfTagInContact),
-          ...this.contacts[indexOfContact].tags.slice(indexOfTagInContact+1)]
+
+      // Start of filtered list
+      const start = this.contacts[indexOfContact]?.tags.slice(0, indexOfTagInContact)
+      // End of filtered list
+      const end = this.contacts[indexOfContact].tags.slice(indexOfTagInContact + 1)
+      // merging start and end
+      this.contacts[indexOfContact].tags = [...start, ...end]
     }
   },
   getters: {
